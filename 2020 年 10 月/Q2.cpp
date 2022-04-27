643bcb3285c16e4b4d416ab12d4415599b86e475
戚祐寧 <samuelchi861008@gmail.com>
2022-04-27 14:59:20 +0800

    更改排版

--- 2020 年 10 月/Q2.cpp
+++ 2020 年 10 月/Q2.cpp
@@ -16,7 +16,7 @@ signed main()
 
 	cin >> R >> C >> k >> m; // 輸入 R C k m
 
-    // 根據 R C 建立各城市的人口
+    	// 根據 R C 建立各城市的人口
 	for(int i=0; i<R; i++) {
 		vector<int> sub;
 		for(int j=0; j<C; j++) {
@@ -27,29 +27,29 @@ signed main()
 		ground.push_back(sub);
 	}
 
-    // 進行 m 天
+    	// 進行 m 天
 	for(int day=0; day<m; day++) {
-        // 需建立一個暫時紀錄用的矩陣，並初始化為 0
+        	// 需建立一個暫時紀錄用的矩陣，並初始化為 0
 		vector<vector<int> > temp(R, vector<int>(C, 0));
 		for(int i=0; i<R; i++) {
 			for(int j=0; j<C; j++) {
 				if(ground[i][j] != -1) {
-                    // 判斷上方是否有值
+                    			// 判斷上方是否有值
 					if(i-1 >= 0 && ground[i-1][j] != -1) {
 						temp[i-1][j] += (ground[i][j] / k);
 						temp[i][j] -= (ground[i][j] / k);
 					}
-                    // 判斷下方是否有值
+                    			// 判斷下方是否有值
 					if(i+1 < R && ground[i+1][j] != -1) {
 						temp[i+1][j] += (ground[i][j] / k);
 						temp[i][j] -= (ground[i][j] / k);
 					}
-                    // 判斷左方是否有值
+                    			// 判斷左方是否有值
 					if(j-1 >= 0 && ground[i][j-1] != -1) {
 						temp[i][j-1] += (ground[i][j] / k);
 						temp[i][j] -= (ground[i][j] / k);
 					}
-                    // 判斷右方是否有值
+                    			// 判斷右方是否有值
 					if(j+1 < C && ground[i][j+1] != -1) {
 						temp[i][j+1] += (ground[i][j] / k);
 						temp[i][j] -= (ground[i][j] / k);	
@@ -57,7 +57,7 @@ signed main()
 				}
 			}
 		}
-        // 根據所記錄的內容更改原始的值
+        	// 根據所記錄的內容更改原始的值
 		for(int i=0; i<R; i++) {
 			for(int j=0; j<C; j++) {
 				if(ground[i][j] != -1) {
@@ -67,7 +67,7 @@ signed main()
 		}
 	}
 
-    // 從矩陣中找到最大最小值，記得略過 -1
+    	// 從矩陣中找到最大最小值，記得略過 -1
 	int MIN = LLONG_MAX, MAX = LLONG_MIN;
 	for(int i=0; i<R; i++) {
 		for(int j=0; j<C; j++) {
@@ -78,7 +78,7 @@ signed main()
 		}
 	}
 
-    // 輸出最小與最大值
+    	// 輸出最小與最大值
 	cout << MIN << "\n" << MAX << endl;
 
 	return 0;
