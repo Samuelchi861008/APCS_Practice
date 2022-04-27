97efc93fea7aeb01098bec0d8cb90959d064a6af
戚祐寧 <samuelchi861008@gmail.com>
2022-04-27 14:55:28 +0800

    更改排版

--- 2017 年 10 月/Q2.cpp
+++ 2017 年 10 月/Q2.cpp
@@ -16,12 +16,12 @@ signed main()
 
 	cin >> k >> s; // 輸入 k 與 s
 
-    // 將字串中每個字元，大寫字母改為 1 小寫字母改為 0
+    	// 將字串中每個字元，大寫字母改為 1 小寫字母改為 0
 	for(int i=0; i<s.length(); i++) {
 		s[i] = (s[i] >= 'A' && s[i] <= 'Z') ? '1' : '0';
 	}
 
-    // 計算連續字母個數
+    	// 計算連續字母個數
 	for(int i=1; i<s.length(); i++) {
 		if((s[i] != s[i-1])) {
 			stored.push_back(total);
@@ -34,29 +34,29 @@ signed main()
 		}
 	}
 
-    // 判斷個數
+    	// 判斷個數
 	int lengthOfString = 0, MAX = 0;
 	for(int i=0; i<stored.size(); i++) {
 		if(stored[i] > k) {
-            // 個數若大於 k 則表示必定包含 k 且交錯字串可能為兩頭
+            		// 個數若大於 k 則表示必定包含 k 且交錯字串可能為兩頭
 			lengthOfString += k;
 			MAX = max(MAX, lengthOfString);
 			lengthOfString = k;
 		} else if(stored[i] == k) {
-            // 個數若等於 k 則表示包含 k
+            		// 個數若等於 k 則表示包含 k
 			lengthOfString += k;
 			MAX = max(MAX, lengthOfString);
 		} else {
-            // 個數若小於 k 則表示不包含 k 且需要重新計算交錯字串
+            		// 個數若小於 k 則表示不包含 k 且需要重新計算交錯字串
 			lengthOfString = 0;
 			continue;
 		}
 	}
 
-    // 假設 k 為 1 且只有單一字元，最大值則為 1
+    	// 假設 k 為 1 且只有單一字元，最大值則為 1
 	if(k==1 && s.length()==1) MAX = s.length();
 
-    // 輸出最大值
+    	// 輸出最大值
 	cout << MAX << endl;
 
 	return 0;
