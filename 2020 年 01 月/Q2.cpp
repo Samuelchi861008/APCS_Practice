5d0a3cd0f44f3ec428faed800ee9b0b64015defa
戚祐寧 <samuelchi861008@gmail.com>
2022-04-27 14:56:58 +0800

    更改排版

--- 2020 年 01 月/Q2.cpp
+++ 2020 年 01 月/Q2.cpp
@@ -16,7 +16,7 @@ signed main()
 
 	cin >> s >> t >> n >> m >> r; // 根據題意輸入 s t n m r
 
-    // 依照 s t 建立矩陣 A 之內容
+    	// 依照 s t 建立矩陣 A 之內容
 	for(int i=0; i<s; i++) {
 		vector<int> sub;
 		for(int j=0; j<t; j++) {
@@ -27,7 +27,7 @@ signed main()
 		A.push_back(sub);
 	}
 
-    // 依照 n m 建立矩陣 B 之內容
+    	// 依照 n m 建立矩陣 B 之內容
 	for(int i=0; i<n; i++) {
 		vector<int> sub;
 		for(int j=0; j<m; j++) {
@@ -38,19 +38,19 @@ signed main()
 		B.push_back(sub);
 	}
 
-    // 將兩個矩陣疊起來，並整個掃一遍
+    	// 將兩個矩陣疊起來，並整個掃一遍
 	for(int i=0; i<=n-s; i++) {
 		for(int j=0; j<=m-t; j++) {
 			int diff = 0, totalOfA = 0, totalOfB = 0;
 			for(int k=0; k<s; k++) {
 				for(int l=0; l<t; l++) {
-                    // 計算子矩陣有多少值與矩陣A不一樣，同時將矩陣A與子矩陣加總
+                    			// 計算子矩陣有多少值與矩陣A不一樣，同時將矩陣A與子矩陣加總
 					if(B[i+k][j+l] != A[k][l]) diff++;
 					totalOfA += A[k][l];
 					totalOfB += B[i+k][j+l];
 				}
 			}
-            // 若子矩陣與矩陣A距離不超過 r 則計算兩相減後並比較最小值，並且計算有多少子矩陣
+            		// 若子矩陣與矩陣A距離不超過 r 則計算兩相減後並比較最小值，並且計算有多少子矩陣
 			if(diff <= r) {
 				int total = (totalOfA - totalOfB < 0) ? ((totalOfA - totalOfB) * -1) : (totalOfA - totalOfB);
 				MIN = min(MIN, total);
@@ -59,7 +59,7 @@ signed main()
 		}
 	}
 
-    // 最後輸出符合條件的子矩陣個數與最小值
+    	// 最後輸出符合條件的子矩陣個數與最小值
 	cout << numOfDiff << "\n" << ((MIN==LLONG_MAX) ? -1 : MIN) << endl;
 
 	return 0;
