18ff342a53cab07176670486ccf079f14af95e85
戚祐寧 <samuelchi861008@gmail.com>
2022-04-27 14:54:36 +0800

    更改排版

--- 2017 年 03 月/Q2.cpp
+++ 2017 年 03 月/Q2.cpp
@@ -15,7 +15,7 @@ signed main()
 
 	cin >> N; // 輸入團體中人數
 
-    // 依序輸入每個人的好友編號，矩陣的 index 為自己編號，內容為好友編號
+    	// 依序輸入每個人的好友編號，矩陣的 index 為自己編號，內容為好友編號
 	for(int i=0; i<N; i++) {
 		int id;
 		cin >> id;
@@ -24,7 +24,7 @@ signed main()
 
 	vector<int> hasGroup(N, 0); // 紀錄每個人是否有被分配到小群體之中
 	for(int i=0; i<hasGroup.size(); i++) {
-        // 當有人未被分配到小群體之中則以他為起點進行分配，直到回到自己為一個小群體
+        	// 當有人未被分配到小群體之中則以他為起點進行分配，直到回到自己為一個小群體
 		if(hasGroup[i] == 0) {
 			int currentIndex = i;
 			while(hasGroup[currentIndex] == 0) {
@@ -35,7 +35,7 @@ signed main()
 		}
 	}
 
-    // 輸出小群體個數
+    	// 輸出小群體個數
 	cout << numOfGroups << endl;
 
 	return 0;
